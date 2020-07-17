6134d8eeea9f714707f95bdd810f7f243d0bed61
qingnan.kong <bellakongqn@gmail.com>
2020-07-17 09:44:34 +0800

    35. 搜索插入位置

---
+++ everyDay/search-insert-position.js
@@ -0,0 +1,26 @@
+/*
+35. 搜索插入位置
+给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。
+如果目标值不存在于数组中，返回它将会被按顺序插入的位置。
+你可以假设数组中无重复元素。
+示例 1:
+输入: [1,3,5,6], 5
+输出: 2
+ */ 
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    let idx = nums.indexOf(target);
+    if(idx >= 0){
+        return idx;
+    }
+    for(let i = 0; i < nums.length; i++){
+        if(nums[i] > target){
+            return i;
+        }
+    }
+    return nums.length;
+};
